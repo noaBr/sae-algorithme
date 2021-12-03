a6063608ceb6fde8ea29d2c21baa6e947e585cad
noaBr <87670992+noaBr@users.noreply.github.com>
2021-12-03 16:16:46 +0100

    Update cryptage.py

--- cryptage.py
+++ cryptage.py
@@ -10,3 +10,34 @@ def list_prime(n) :
 			if test==False :
 				l=l+[i] 
 	return l
+
+
+
+def qotrem(a,b):
+	q=0
+	r=a
+	while r>=b:
+		q=q+1
+		r=r-b
+	return q,r
+
+def pgcd_it(a,b):
+	r=qotrem(a,b)[1]
+	while r!=0:
+		a=b
+		b=r
+		r=qotrem(a,b)[1]
+	return b
+
+def key_creation() :
+	p = choice(list_prime(200))
+	q = choice(list_prime(200))
+	n = p*q
+	phi = (p-1)*(q-1)
+	test = False
+	while test == False :
+		e = randint(3,200)
+		if pgcd_it(phi,e)==1:
+			pub = e
+			test = True
+	return phi, pub
